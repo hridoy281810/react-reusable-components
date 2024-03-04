4d824cf18a6292ebba5e379ef3e9bde8eb08d5a5
hridoy281810 <anisurrahman01815001904@gmail.com>
2024-03-04 11:51:14 +0600

    compleate reusable form

--- src/components/ReusableForm/Input.tsx
+++ src/components/ReusableForm/Input.tsx
@@ -5,7 +5,7 @@ export const Input = ({errors,register,type,label}) => {
     <div className='w-full max-w-md'>
     <label className='block'> {label} </label>
     <input type={type}   id='name ' {...register}/>
-    {errors && <span className='text-xs text-red-600'>{errors}</span>}
+    {errors.name && <span className='text-xs text-red-600'>{errors.name.message}</span>}
 
 </div>
   );
